bd96344f3237bb9b75a8f5cc0e62c1092b4c425a
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-10-26 16:59:26 +0900

    feat : setting

---
+++ src/main/java/com/example/yoon_cicd/YoonCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoon_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yoon_cicd/YoonCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yoon_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
